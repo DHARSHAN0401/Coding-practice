3f83b4a92a383b2b665c2c275c70e2480506b213
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-18 20:45:35 +0530

    Add files via upload

---
+++ practice 3- check prime number.py
@@ -0,0 +1,18 @@
+def is_prime(num):
+    
+    # Prime numbers start from 2
+    if num < 2: 
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        
+    # Check divisors up to √num
+        if num % i == 0:
+            return False
+    return True
+
+# Input from the user
+num = int(input("Enter a number to check is prime or not: "))
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
