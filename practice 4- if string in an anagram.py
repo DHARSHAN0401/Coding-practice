f277f9de695df32040bbcc5a6e11d3dc8980e68e
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-18 21:50:23 +0530

    Add files via upload

---
+++ practice 4- if string in an anagram.py
@@ -0,0 +1,15 @@
+#Q4 string is an anagram :
+
+def is_anagram(str1, str2):
+    
+    # Sort both strings and compare
+    return sorted(str1) == sorted(str2)
+
+    g # Inputs
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+
+if is_anagram(word1, word2):
+    print(f"{word1} and {word2} are anagrams.")
+else:
+    print(f"{word1} and {word2} are not anagrams.")
