8440205e701a99f399768126d38ed729ed210176
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-18 14:56:32 +0530

    Add files via upload

---
+++ Practice 2 -Data type conversions.py
@@ -0,0 +1,17 @@
+#Q2 data types conversions :
+
+# Input a number as a string
+num_str = input("Enter a number: ")
+
+# Converting string to integer
+num_int = int(num_str)
+
+# Converting integer to float
+num_float = float(num_int)
+
+# Converting back to string
+num_str_again = str(num_float)
+
+print(f"String to Integer: {num_int}")
+print(f"Integer to Float: {num_float}")
+print(f"Float to String: {num_str_again}")
---
+++ practice 1-display the user input.py
@@ -0,0 +1,8 @@
+#Q1 simple user input data was display in format
+
+# Prompting the user for input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Formatting the output with f-strings
+print(f"Hi....{name} You are {age} years old.")
