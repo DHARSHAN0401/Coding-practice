87ff1aeb16f0ad9d7bfbccb3272ab249e1df2532
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-28 01:35:49 +0530

    Add files via upload

---
+++ practice-7 relate between two numbers.py
@@ -0,0 +1,37 @@
+#Q7 related the two numbers by using set of special tools.
+
+def compare_two_numbers():
+    num1=int(input("enter 1st number : "))
+    num2=int(input("enter 2nd number : "))
+    
+    print((num1),">",(num2),"is true")
+    print((num1),"<=",(num2),"is true")
+    print((num1),">",(num2),"is false")
+    print((num1),">=",(num2),"is false")
+    print((num1),"=",(num2),"is false")
+    print((num1),"not equal",(num2),"is true")
+
+# call the function.
+compare_two_numbers()
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
