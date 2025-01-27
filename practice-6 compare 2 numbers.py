4b86a5ea0a3d465fb1b60c499f41933380b31b56
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-28 01:11:46 +0530

    Add files via upload

---
+++ practice-6 compare 2 numbers.py
@@ -0,0 +1,14 @@
+def find_bignumber():
+    num1=float(input("1st number:"))
+    num2=float(input("2nd number:"))
+    if num1>num2:
+        print("biggest number is:",("num1"))
+    elif num1<num2:
+        print("biggest number is:",("num2"))
+    else :
+        print("both numbers are equal")
+        
+# call the functions      
+find_bignumber()
+
+        
