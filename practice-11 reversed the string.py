fd2e71f876239620972d1ede1e57e3c852c47776
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-29 02:36:15 +0530

    Add files via upload

---
+++ practice-11 reversed the string.py
@@ -0,0 +1,9 @@
+#Q11 reverse the string.
+
+def reverse_string():
+    word="hello world"
+    reverse_word= word[::-1]
+    print((word),"is  reveresd as",(reverse_word))
+    
+#call the function.
+reverse_string()
