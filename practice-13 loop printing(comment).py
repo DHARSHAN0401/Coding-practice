ba291331fb76e00ad50803b429ab410ecb1c955a
Dharshan J <153801328+DHARSHAN0401@users.noreply.github.com>
2025-01-30 23:56:30 +0530

    Add files via upload

---
+++ practice-13 loop printing(comment).py
@@ -0,0 +1,9 @@
+#Q14 loop printing of exactly 20 times in certain comment("all is well").
+
+def loop_printing():
+    num=int(input("enter the number:"))
+    for i in range(20):
+        print("all is well")
+
+#call function:
+loop_printing()
