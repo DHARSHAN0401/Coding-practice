3afbcbb485ce0ff61e423aa5f86fc983015430b5
DHARSHAN0401 <dharshanb.e1328@gmail.com>
2025-01-31 08:56:00 +0530

    Resolved merge conflicts

--- practice 1-display the user input.py
+++ practice 1-display the user input.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Q1 simple user input data was display in format .
 
 # Prompting the user for input
@@ -6,3 +7,13 @@ age = input("Enter your age: ")
 
 # Formatting the output with f-strings
 print(f"Hi....{name} You are {age} years old.")
+=======
+#Q1 simple user input data was display in format
+
+# Prompting the user for input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Formatting the output with f-strings
+print("Hi....",(name)," You are ",(age)," years old.")
+>>>>>>> ab786f1 (Initial commit)
