6ebaeb76cf6369fdcbec8aa011c29552d7a08788
DHARSHAN0401 <dharshanb.e1328@gmail.com>
2025-01-31 09:36:34 +0530

    update

--- practice-18 fizz buzz in interger.py
+++ practice-18 fizz buzz in interger.py
@@ -3,14 +3,14 @@
 def fizz_buzz():
     num=int(input("enter the numbers :"))
     for i in range(1,num+1):
-        if i%3==0:
-            print("fizz",end="")
+        if i%3==0 and i%5==0:
+            print("fizz buzz")
+        elif i%3==0:
+            print("fizz")
         elif i%5==0:
-            print("buzz",end="")
-        elif i%3==0 and i%5==0:
-            print("fizz buzz", end="")
+            print("buzz")
         else:
-            print(str(i),end="")
+            print(str(i))
 
 #call function:
 fizz_buzz()
