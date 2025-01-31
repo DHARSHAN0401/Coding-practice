2f16083983b3b6ce80a053f0b569906bea894098
DHARSHAN0401 <dharshanb.e1328@gmail.com>
2025-01-31 09:01:48 +0530

    #made changes

--- Practice 2 -Data type conversions.py
+++ Practice 2 -Data type conversions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Q2 data types conversions :
 
 # Input a number as a string
@@ -13,25 +12,6 @@ num_float = float(num_int)
 # Converting back to string
 num_str_again = str(num_float)
 
-print(f"String to Integer: {num_int}")
-print(f"Integer to Float: {num_float}")
-print(f"Float to String: {num_str_again}")
-=======
-#Q2 data types conversions :
-
-# Input a number as a string
-num_str = input("Enter a number: ")
-
-# Converting string to integer
-num_int = int(num_str)
-
-# Converting integer to float
-num_float = float(num_int)
-
-# Converting back to string
-num_str_again = str(num_float)
-
-print(f"String to Integer: {num_int}")
-print(f"Integer to Float: {num_float}")
-print(f"Float to String: {num_str_again}")
->>>>>>> ab786f1 (Initial commit)
+print("String to Integer:" ,(num_int))
+print("Integer to Float: ",(num_float))
+print("Float to String: ",(num_str_again))
--- practice 1-display the user input.py
+++ practice 1-display the user input.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-#Q1 simple user input data was display in format .
-
-# Prompting the user for input
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-
-# Formatting the output with f-strings
-print(f"Hi....{name} You are {age} years old.")
-=======
 #Q1 simple user input data was display in format
 
 # Prompting the user for input
@@ -16,4 +6,4 @@ age = input("Enter your age: ")
 
 # Formatting the output with f-strings
 print("Hi....",(name)," You are ",(age)," years old.")
->>>>>>> ab786f1 (Initial commit)
+
